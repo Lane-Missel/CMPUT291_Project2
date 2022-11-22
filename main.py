45ae065a29a0828acde936b663f7d66567511b41
Lane Missel <lane.missel@shaw.ca>
2022-11-21 22:35:39 -0700

    Implement interface. Add test return values to incomplete functions.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import sys
 class DatabaseManager:
 
     def __init__(self, port: int):
-        self.client = MongoClient()
+        self.client = MongoClient("localhost://")
+        self.collection = self.client["dblp"]
 
     def search_articles(self, keywords: list[str]) -> list[dict[str: str, str: str, str: list[str]], str: str, str: str, str: int]:
         """
@@ -12,11 +13,10 @@ class DatabaseManager:
         The return will be a list of dictionaries in the format:
             {'id': <str>,
             'title': <str>,
-            'authors': [<str>,],
-            'abstract': <str>,
             'venue': <str>,
             'year': <int>}1
         """
+        return [{"id": "AAAABBBB", "title": "Test Paper", "venue": "Big Test Venue", "year": 2022}, {"id": "DDDDBBBB", "title": "Another Test Paper", "venue": "Another Test Venue", "year": 2018}]
 
     def search_authors(self, keyword: str) -> list[list[str, int]]:
         """
@@ -26,24 +26,45 @@ class DatabaseManager:
         assert(len(keyword) > 0)
         assert(isinstance(keyword, str))
 
+        return [["Test Author", 22], ["Another Author", 10]]
+
     def top_venues(self, n: int):
         """
         Returns the top n venues, based on the number of articles that reference the venue.
         The return will be a list of dictionaries in the format:
-            [[<venue1>, <BiggestArticleCount>], [<venue2>, <SmallerArticelCount>]]
+            [[<venue1>, <BiggestArticleCount>, <ReferenceCount>], [<venue2>, <SmallerArticelCount>, <RefercenCount>]]
         """
         assert(n > 0)
         assert(isinstance(n, int))
+
+        return [["Test Venue", 4, 16], ["Another Venue", 2, 12]]
 
     def add_article(self, identifier: str, title: str, authors: list[str], year: int):
         """
         Adds an article to the mongo database.
         """
+        return
+
+    def find_article(self, article_id: str) -> dict:
+        """
+        return sdictionary containing information of article with provided id.
+        """
+        return self.collection.find({"id": article_id}, {})[0]
+
+    def articles_by_author(self, author: str) -> list[dict]:
+        """
+        title, venue, and year (sorted by year descending)
+        """
+        return [["Test Paper", "A Big Venue", 2022],["Another Paper", "Smaller Venue", 2017]]
+
+    def has_key(self, key: str) -> bool:
+        return len(self.collection.find({"id": key},{})) == 1
 
 class Interface:
 
     def __init__(self, database: MongoClient):
         self.database = database
+        self.collection = self.database["dblp"]
 
     def selection(self):
         """
@@ -74,7 +95,7 @@ class Interface:
             print("No keywords entered. Returning to main.")
             return
 
-        articles = self.database.search_for_articles(keywords)
+        articles = self.database.search_articles(keywords)
 
         if len(articles) == 0:
             print("No articles found containing keywords: ", end="")
@@ -84,16 +105,152 @@ class Interface:
 
             return
 
-        # display articles (allow user to pick them)...  
+        # display articles (allow user to pick them)...
+        print("{} {} {} {} {}".format(" ", "id", "title", "year", "venue"))
+        num_articles = len(articles)
+
+        for i in range(num_articles):
+            article = articles[i]
+            print("entry {}:".format(i + 1))
+            print("id: {}".format(article["id"]))
+            print("title: {}".format(article[i]))
+            print("year: {}".format(article["year"]))
+            print("venue: {}".format(article["venue"]))
+            print("- -" * 5)
+
+        valid_choice = False
+        while not valid_choice:
+            user_input = input("Enter entry number for the artice you would like to choose (or 0 to exit): ")
+
+            article_index = None
+            try:
+                article_index = int(user_input) - 1
+            except Exception:
+                print("Invalid entry. Try again.")
+
+            if article_index in range(num_articles):
+                valid_choice = True
+
+        article_id = articles[article_index]["id"]
+        article = self.database.find_article(article_id)
+
+        # Display the choosen article:
+        print("Article with id: {}".format(article["id"]))
+        print("title: {}".format(article["title"]))
+        print("year: {}".format(article['year']))
+        print("venue: {}".format(article["venue"]))
+        print("authors: ", end="")
+        
+        for author in article["authors"]:
+            print(author, end=" ")
+
+        print("references:")
+
+        for ref_article_id in article["references"]:
+            print("Article with id {}:".format(ref_article_id))
+            ref_article = self.database.find_article(ref_article_id)
+            print("title: {}".format(ref_article_id))
+            print("year: {}".format(ref_article["year"]))
+            print("* *" * 5)
+
+        return
 
     def search_for_authors(self):
-        pass
+        keyword = input("Enter keyword: ").strip()
 
+        if len(keyword) == 0:
+            print("No keyword entered.")
+            return
+
+        authors = self.database.search_authors(keyword)
+
+        if len(authors) == 0:
+            print("No authors found.")
+            return
+
+        print("[0] Return to menu")
+        num_authors = len(authors)
+        for i in range(num_authors):
+            author = authors[i]
+            print("[{}] {} - {} publications".format(i + 1, author[0], author[1]))
+
+        valid_selection = False
+        while not valid_selection:
+            selection = input("Selection: ").strip()
+
+            if len(selection) == 0:
+                print("No artist selected. Try again.")
+                continue
+
+            try:
+                author_index = int(selection) - 1
+            except Exception:
+                print("Invalid selection. Try again.")
+                continue
+
+            if author_index not in range(-1, num_authors):
+                print("Invalid selection. Try again.")
+                continue
+
+            valid_selection = True
+
+        # display selected author.            
+        if author_index < 0:
+            return
+
+        # display publications by author
+        for entry in self.database.articles_by_author(authors[author_index][0]):
+            print("{} - {} - {}".format(entry[0], entry[1], entry[2]))
+        
     def list_the_venues(self):
-        pass
+        valid_number = False
+        while not valid_number:
+            try:
+                num_venues = int(input("Top n venues? ").strip())
+            except Exception:
+                print("invalid selection. Try again.")
+
+            if num_venues < 1:
+                print("cannot return negive amount of venues. Back to home.")
+                return
+
+            valid_number = True
+
+        # get venues
+        venues = self.database.top_venues(num_venues)
+
+        for i in range(len(venues)):
+            venue = venues[i]
+            print("{}. {} hosted {} articles and references {}".format(i+1, venue[0], venue[1], venue[2]))
+
+        return
 
     def add_an_article(self):
-        pass
+        unique_id = input("Enter id: ")
+        
+        if self.database.has_key(unique_id):
+            print("This id already exists. Back to Home.")
+            return
+
+        title = input("Title: ")
+
+        if len(title) < 1:
+            print("Title cannot be empty. Back to Home")
+            return
+
+        authors = input("Authors: ").split(",")
+        
+        if len(authors) < 1:
+            print("Must be atleast one author. Bc to Home.")
+            return
+
+        year = input("Year: ")
+        try:
+            year = int(year)
+        except Exception:
+            print("Invalid year entered. Back to Home.")
+
+        self.database.add_article(unique_id, title, authors, year)
 
     def run(self):
         print("Welcome!")
