ce8311fdfad3086917b631d813376a33b72b7f7c
Lane Missel <58617816+Lane-Missel@users.noreply.github.com>
2022-11-23 14:56:56 -0700

    rollback documention

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class DatabaseManager:
         self.client = MongoClient("mongodb://localhost:{}".format(port))
         self.collection = self.client.get_database("291db").get_collection("dblp")
 
-    def search_articles(self, keywords: list[str]) -> List[Dict[str: str, str: str, str: List[str], str: str, str: str, str: int]]:
+    def search_articles(self, keywords: list[str]) -> list:
         """
         Returns all articles that have a keyword in the title, authors, abstract, venue or year.
         The return will be a list of dictionaries in the format:
@@ -22,7 +22,7 @@ class DatabaseManager:
         return list(self.collection.find( {'$text': { '$search': keywords } } ))
         #return [{"id": "AAAABBBB", "title": "Test Paper", "venue": "Big Test Venue", "year": 2022}, {"id": "DDDDBBBB", "title": "Another Test Paper", "venue": "Another Test Venue", "year": 2018}]
 
-    def search_authors(self, keyword: str) -> List[List[str, int]]:
+    def search_authors(self, keyword: str) -> list:
         """
         Returns all authors whose name includes the provided keyword.
         in form [[<author1>,<publications>],]
